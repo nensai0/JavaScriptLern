42f41e3368e7c6e260ee46e722dc692311c3fdac
Artem Horbeshko <artemhorbeshko777@gmail.com>
2025-12-14 13:14:40 +0100

    f

---
+++ js03/script.js
@@ -0,0 +1,40 @@
+let count = 0;
+let count1 = 0;
+
+// while (count < 3) {
+//     console.log(count);
+//     count++;
+// }
+
+// do {
+//     console.log(count1);
+//     count1++;
+// } while (count1 < 3);
+
+// for (let i = 3; i >= 0; i--) {
+//     console.log(i)
+// }
+
+// const arr1 = [ 'Kiev', 'Kharkiv', 'Odesa', 'Dnipro']
+
+// for (let i = 0; i <=3; i++) {
+//     console.log(arr1[i])
+// }
+
+const object = {name: "Artem", age: 27};
+const arr = [ 'Kiev', 'Kharkiv', 'Odesa', 'Dnipro'];
+
+// for (const key in object) {
+//     console.log(key);
+//     console.log(object[key]);
+// }
+
+// for (const element of arr) {
+//     console.log(element);
+// }
+
+a: for (let i = 1; i <= 9; i++) {
+   b: for (let j = 1; j <= 9; j++) {
+    console.log(`${i} * ${j} = ${i*j}`)
+   }   
+}
