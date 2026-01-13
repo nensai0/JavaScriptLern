349438f1cbd95d2d7c76c69dac9d2d5cbd853c4e
Artem Horbeshko <artemhorbeshko777@gmail.com>
2026-01-13 15:01:47 +0100

    js10

--- js06hw/script02.js
+++
@@ -1,9 +0,0 @@
-const arr1 = [1, 2, 3, 4, 5]; 
-const arr2 = [3, 4, 5, 6, 7]; 
-
-function greed (arr1, arr2) {
-    return arr = new Set([...arr1, ...arr2])
-    // return arr = [new Set(arr1.concat(arr2))]
-}
-
-console.log(greed(arr1, arr2))
